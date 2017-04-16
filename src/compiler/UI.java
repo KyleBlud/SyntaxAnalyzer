d7c8df42dc2377d314796fde6818797f5f6da453
KyleBlud <kylebludworth@outlook.com>
2017-04-15 23:00:30 -0400

    Went back to JFrame Panel
    I figured it would make it easier to grab test files and also get that
    part of the project out of the way. Whoo.

---
+++ src/compiler/UI.java
@@ -0,0 +1,181 @@
+package compiler;
+
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class UI extends javax.swing.JFrame {
+
+    Tokenizer t;
+    
+    /**
+     * Creates new form UI
+     */
+    public UI() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        uploadFilePanel = new javax.swing.JPanel();
+        uploadFileButton = new javax.swing.JButton();
+        fileNameLabel = new javax.swing.JLabel();
+        resultsPanel = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        uploadFilePanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Upload program text file"));
+        uploadFilePanel.setLayout(new java.awt.GridBagLayout());
+
+        uploadFileButton.setText("Upload");
+        uploadFileButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                uploadFileButtonActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(24, 107, 27, 0);
+        uploadFilePanel.add(uploadFileButton, gridBagConstraints);
+
+        fileNameLabel.setText("No file chosen.");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(29, 6, 0, 104);
+        uploadFilePanel.add(fileNameLabel, gridBagConstraints);
+
+        resultsPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Results"));
+
+        javax.swing.GroupLayout resultsPanelLayout = new javax.swing.GroupLayout(resultsPanel);
+        resultsPanel.setLayout(resultsPanelLayout);
+        resultsPanelLayout.setHorizontalGroup(
+            resultsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        resultsPanelLayout.setVerticalGroup(
+            resultsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 96, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(resultsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(uploadFilePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 400, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(uploadFilePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(resultsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void uploadFileButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_uploadFileButtonActionPerformed
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter;
+        filter = new FileNameExtensionFilter("Text files", "txt", "text");
+        chooser.setFileFilter(filter);
+
+        int returnVal;
+        boolean correctFileType = false;
+        while (!correctFileType)
+        {
+            returnVal = chooser.showOpenDialog(this);
+            if (returnVal == chooser.APPROVE_OPTION)
+            {
+                File programFile = chooser.getSelectedFile();
+                correctFileType = filter.accept(programFile);
+                if (correctFileType)
+                {
+                    fileNameLabel.setText(programFile.getName());
+                    try
+                    {
+                        /*
+                        When the tokenizer is finished, replace this line 
+                        with the parser and pass the file to the parser. 
+                        The parser will then pass the file as a scanner
+                        object to the tokenizer. The tokenizer will then return
+                        an array of tokens.
+                        */
+                        t = new Tokenizer(new Scanner(programFile));
+                    }
+                    catch (Exception e)
+                    {
+                        JOptionPane.showMessageDialog(null, e);
+                    }
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null, "Please select text (.txt) files only.");
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }//GEN-LAST:event_uploadFileButtonActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new UI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel fileNameLabel;
+    private javax.swing.JPanel resultsPanel;
+    private javax.swing.JButton uploadFileButton;
+    private javax.swing.JPanel uploadFilePanel;
+    // End of variables declaration//GEN-END:variables
+}
