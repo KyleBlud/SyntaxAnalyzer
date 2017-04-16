5b3ce8db90cd3ffbc47558e07fd159f215f9862c
KyleBlud <kylebludworth@outlook.com>
2017-04-16 17:51:23 -0400

    Able to tokenize a program file
    The tokenizeFile method is now able to go line by line and
    differentiate tokens and add them to an ArrayList.

--- src/compiler/Tokenizer.java
+++ src/compiler/Tokenizer.java
@@ -1,6 +1,7 @@
 package compiler;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Tokenizer
@@ -13,20 +14,87 @@ public class Tokenizer
         this.sc = sc;
         tokens = new ArrayList<>();
         tokenizeFile();
+        printTokens();
     }
     
     private void tokenizeFile()
     {
         String line;
+        char currChar;
+        String token = "";
+        
         while (sc.hasNextLine())
         {
-            line = sc.nextLine().replaceAll("\\s+", "");
-            System.out.print(line);
+            line = sc.nextLine();
+            for (int i = 0; i < line.length(); i++)
+            {
+                currChar = line.charAt(i);
+                if (isTerminalSymbol(currChar))
+                {
+                    if (!token.isEmpty())
+                    {
+                        token = addTokenAndClear(token);
+                    }
+                    if (currChar == ':')
+                    {
+                        token += "" + currChar + line.charAt(++i);
+                        token = addTokenAndClear(token);
+                    }
+                    else
+                    {
+                        token += currChar;
+                        token = addTokenAndClear(token);
+                    }
+                }
+                else if (Character.isWhitespace(currChar)) 
+                {
+                    if (!token.isEmpty())
+                    {
+                        token = addTokenAndClear(token);
+                    }
+                }
+                else 
+                {
+                    token += currChar;
+                }
+            }
+            if (!token.isEmpty())
+            {
+                token = addTokenAndClear(token);
+            }
         }
     }
     
     public ArrayList<String> getTokens()
     {
         return tokens;
+    }
+    
+    private boolean isTerminalSymbol(char c)
+    {
+        switch (c) 
+        {
+            case '(': case ')': case ',': 
+            case '+': case '-': case ';':
+            case ':': case '=':
+                return true;
+            default:
+                return false;
+        }
+    }
+    
+    private String addTokenAndClear(String token)
+    {
+        tokens.add(token);
+        return "";
+    }
+    
+    private void printTokens()
+    {
+        Iterator<String> i = tokens.iterator();
+        while (i.hasNext()) 
+        {
+            System.out.println(i.next());
+        }
     }
 }
